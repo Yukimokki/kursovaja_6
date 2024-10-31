26937d14ba51a710de0d6489f8b1a31ac092b53b
Yukimokki <yana.domnina@gmail.com>
2024-10-31 03:27:21 +0300

    Models 1st commit

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from config import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path("", include("mailing.urls", namespace="mailing")),
-    path("mailing/", include("mailing.urls",namespace="mailing"))
+    path("", include("mailing.urls")),
+    #path('mailing/', include("mailing.urls", namespace="mailing"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
